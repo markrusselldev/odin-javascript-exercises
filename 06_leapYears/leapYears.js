089be53fe7f063ba3745c4676f4ffa4d97e7cf2c
markrusselldev <mark@markrussell.dev>
2022-12-13 00:01:03 -0700

    complete: 06_leapYears

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,6 +1,19 @@
-const leapYears = function() {
+const leapYears = function (year) {
+  // if/elseif version
+  // if (year % 4 === 0 && year % 100 !== 0) {
+  //   return true;
+  // } else if (year % 100 === 0 && year % 400 == 0) {
+  //   return true;
+  // }
+  // return false;
 
+  // short version
+  return (year % 4 === 0 && year % 100 !== 0) || year % 400 == 0;
 };
+
+let test1 = leapYears(2000); // is a leap year: returns true
+let test2 = leapYears(1985); // is not a leap year: returns false
+console.log(test1, test2);
 
 // Do not edit below this line
 module.exports = leapYears;
