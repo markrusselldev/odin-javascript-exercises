5b98421aeb14b9b6cba64d5e75e723a9af42165f
markrusselldev <mark@markrussell.dev>
2022-12-12 20:48:59 -0700

    complete: 03_reverseString

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,8 @@
-const reverseString = function() {
-
+const reverseString = function (string) {
+  return string.split("").reverse().join("");
 };
+let test = reverseString("hello there");
+console.log(test);
 
 // Do not edit below this line
 module.exports = reverseString;
