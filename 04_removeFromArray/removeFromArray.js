84db400e8c7c96a281febd6bfc3a73396add7b1e
markrusselldev <mark@markrussell.dev>
2022-12-12 21:35:00 -0700

    complete: 04_removeFromArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,11 @@
-const removeFromArray = function() {
-
+const removeFromArray = function (array, ...elements) {
+  // console.log("array: ", array);
+  // console.log("elements: ", elements);
+  return array.filter(element => !elements.includes(element));
 };
+
+let test = removeFromArray([1, 2, 3, 4], 3, 2);
+console.log(test);
 
 // Do not edit below this line
 module.exports = removeFromArray;
