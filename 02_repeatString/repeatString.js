286f0bdb30853b63029dd612f5668c3a46a692a5
markrusselldev <mark@markrussell.dev>
2022-12-12 20:39:49 -0700

    complete: 01_helloWorld, 02_repeatString

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,16 @@
-const repeatString = function() {
+const repeatString = function (string, num) {
+  if (num === 0) return "";
+  if (num < 0) return "ERROR";
+  let result = "";
 
+  while (num >= 1) {
+    result += string;
+    num--;
+  }
+  console.log(result);
+  return result;
 };
 
+console.log(repeatString("hey", 3));
 // Do not edit below this line
 module.exports = repeatString;
