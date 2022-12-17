ca1ca82f031fbd3ee066edc674889ddff3fd5c2b
markrusselldev <mark@markrussell.dev>
2022-12-17 15:17:15 -0700

    complete: 08_calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,41 @@
-const add = function() {
-	
+const add = function (a, b) {
+  return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function (a, b) {
+  return a - b;
 };
 
-const sum = function() {
-	
+const sum = function (arr) {
+  return arr.reduce((last, next) => last + next, 0);
 };
 
-const multiply = function() {
-
+const multiply = function (arr) {
+  return arr.reduce((last, next) => last * next, 1);
 };
 
-const power = function() {
-	
+const power = function (a, b) {
+  return a ** b;
 };
 
-const factorial = function() {
-	
+const factorial = function (n) {
+  if (!Number.isInteger(n)) return undefined;
+  if (n < 0) return undefined;
+  let result = 1;
+  for (i = n; i >= 1; i--) {
+    result = result * i;
+  }
+  return result;
+};
+
+const factorial2 = n => {
+  if (!Number.isInteger(n)) return undefined;
+  if (n < 0) return undefined;
+  if (n === 0) return 1;
+  // adds numbers from 1 to n to array
+  let arr = [...Array(n + 1).keys()].splice(1);
+  // reduce is much cleaner
+  return arr.reduce((last, next) => last * next);
 };
 
 // Do not edit below this line
