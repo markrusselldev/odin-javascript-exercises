96a4c8c70c660a6916703c4802f7df04bd6250d0
markrusselldev <mark@markrussell.dev>
2022-12-17 20:38:20 -0700

    complete: 09_palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,7 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+  // regex wizardry to remove punctuation and all whitespace
+  const cleanString = string.replace(/[^a-z0-9]/gi, "").toLowerCase();
+  return cleanString.split("").reverse().join("") === cleanString;
 };
 
 // Do not edit below this line
