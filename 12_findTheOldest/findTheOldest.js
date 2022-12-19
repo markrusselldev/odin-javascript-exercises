5251ed2ee6a38b4d12ca831abb65955b6ccd0e63
markrusselldev <mark@markrussell.dev>
2022-12-19 14:25:30 -0700

    complete: 12_findTheOldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,13 @@
-const findTheOldest = function() {
-
+const findTheOldest = function (people) {
+  return people.reduce((acc, curr) => {
+    // if person is still living, check against this year
+    if (!acc.yearOfDeath) acc.yearOfDeath = new Date().getFullYear();
+    // no clue if i'm doing this right... but it works
+    if (curr.yearOfDeath - curr.yearOfBirth > acc.yearOfDeath - acc.yearOfBirth) {
+      acc = curr;
+    }
+    return acc;
+  });
 };
 
 // Do not edit below this line
