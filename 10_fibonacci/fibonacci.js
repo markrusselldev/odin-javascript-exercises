0ceee9159e6727c049e4e5a7a59716070a6fe39e
markrusselldev <mark@markrussell.dev>
2022-12-18 17:08:49 -0700

    complete: 10_fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,8 @@
-const fibonacci = function() {
-
+const fibonacci = function (n) {
+  if (n <= 0) return "OOPS";
+  return Array.from({ length: n })
+    .reduce((acc, val, i) => acc.concat(i > 1 ? acc[i - 1] + acc[i - 2] : i === 0 ? i + 1 : i), [])
+    .pop();
 };
 
 // Do not edit below this line
